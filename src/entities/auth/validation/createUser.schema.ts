4b2881e60b220b60c1bd932535dba13daf9292fc
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-04-17 16:25:35 +0300

    update createUserSchema

--- src/entities/auth/validation/createUser.schema.ts
+++ src/entities/auth/validation/createUser.schema.ts
@@ -1,11 +1,10 @@
 import * as yup from 'yup';
-import {ErrorMessages} from '@/const/errors.const';
-import {nameMinMaxLength, passwordValidationSchema, phoneRegex} from '@/entities/auth/const/validation';
+import {nameMinMaxLength, passwordValidationSchema} from '@/entities/auth/const/validation';
 
 export const createUserSchema = yup.object().shape({
   firstName: nameMinMaxLength,
   lastName: nameMinMaxLength,
   email: yup.string().email().required(),
   password: passwordValidationSchema,
-  phoneNumber: yup.string().matches(phoneRegex, ErrorMessages.INVALID_PHONE_FORMAT()).required()
+  phoneNumber: yup.string()
 });
