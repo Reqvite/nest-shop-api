189dcdb949818e3bf4a9765f46bd4e52eb93230e
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-04-22 21:37:23 +0300

    fix import

--- src/entities/auth/const/validation.ts
+++ src/entities/auth/const/validation.ts
@@ -1,7 +1,8 @@
 import * as yup from 'yup';
 import {ErrorMessages} from '@/const/errors.const';
-import {yupValidation} from '@/const/validation.const';
 
+export const nameMaxLength = 30;
+export const nameMinLength = 3;
 export const passwordRegex = /^(?=.*[a-z].*[a-z])(?=.*[A-Z].*[A-Z])(?=.*\d.*\d)(?=.*[^a-zA-Z0-9].*[^a-zA-Z0-9]).*$/;
 export const phoneRegex = /^[0-9]{12}$/;
 export const passwordValidationSchema = yup
@@ -12,4 +13,4 @@ export const passwordValidationSchema = yup
     'Password must contain at least 2 lowercase, 2 uppercase, 2 digits, and 2 special characters'
   )
   .required();
-export const nameMinMaxLength = yupValidation.getMinMaxString({min: 3, max: 30});
+export const nameMinMaxLength = yup.string().min(nameMinLength).max(nameMaxLength).required();
