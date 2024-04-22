8d7546dbe29a7bcf703562306024d6be5e91cf95
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-04-22 14:24:48 +0300

    add return types

--- src/entities/product/product.controller.ts
+++ src/entities/product/product.controller.ts
@@ -2,6 +2,7 @@ import {Body, Controller, Post, UsePipes} from '@nestjs/common';
 import {ApiTags} from '@nestjs/swagger';
 import {YupValidationPipe} from '@/pipes/yupValidation.pipe';
 import {CreateProductDto} from './dto/createProduct.dto';
+import {Product} from './model/product.model';
 import {ProductService} from './product.service';
 import {ProductSwagger} from './swagger/product.swagger';
 import {productSchema} from './validation/product.schema';
@@ -13,7 +14,7 @@ export class ProductController {
   @Post()
   @UsePipes(new YupValidationPipe(productSchema))
   @ProductSwagger.create()
-  async create(@Body() dto: Omit<CreateProductDto, '_id'>) {
+  async create(@Body() dto: Omit<CreateProductDto, '_id'>): Promise<Product> {
     return this.productService.create(dto);
   }
 }
