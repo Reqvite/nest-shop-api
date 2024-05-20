cda10bf32898970270577d3fccefa6b5a47fa63a
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-05-20 22:13:40 +0300

    update

--- src/entities/product/helpers/getQueryParams.ts
+++ src/entities/product/helpers/getQueryParams.ts
@@ -17,17 +17,19 @@ export const getQueryParams = (params: ProductsQueryParamsSchemaType) => {
   try {
     if (brands && Array.isArray(brands)) query.brand = {$in: brands};
     if (tags && Array.isArray(tags)) query.tags = {$in: tags};
-    if (categories && Array.isArray(categories)) query.category = {$in: categories};
+    if (categories && Array.isArray(categories) && categories[0] !== 0) query.category = {$in: categories};
     if (subCategory && Array.isArray(subCategory)) query.subCategory = {$in: subCategory};
     if (prices && prices[0] && prices[1]) query.discountedPrice = {$gte: prices[0] - 1, $lt: prices[1] + 1};
     if (rating && Array.isArray(rating)) query.rating = {$gte: rating[0], $lt: rating[1] + 1};
     if (search) {
-      const options = {$regex: search, $options: 'i'};
+      const options = {$regex: '\\b' + search, $options: 'i'};
       query.$or = [
         {title: options},
         {brand: options},
         {'description.label': options},
-        {'characteristics.label': options}
+        {'description.value': options},
+        {'characteristics.label': options},
+        {'characteristics.value': options}
       ];
     }
   } catch (error) {
@@ -35,5 +37,5 @@ export const getQueryParams = (params: ProductsQueryParamsSchemaType) => {
     throw CustomErrors.BadRequestError(ErrorMessages.INVALID_PARAMS);
   }
 
-  return {query, skip, pageIdx, itemsLimit};
+  return {query, skip, pageIdx, itemsLimit, categories};
 };
