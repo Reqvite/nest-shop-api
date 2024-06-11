f64268a8a36959271499c222872ab1e894cbe69d
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-06-11 13:34:33 +0300

    updated create session api

--- src/entities/stripe/stripe.service.ts
+++ src/entities/stripe/stripe.service.ts
@@ -1,6 +1,7 @@
 import {Inject, Injectable} from '@nestjs/common';
 import {ConfigService} from '@nestjs/config';
 import Stripe from 'stripe';
+import {BillingInfo} from '@/types/cart.interface';
 import {CartService} from '../cart/cart.service';
 import {CreateCheckoutSessionDto} from './dto/createCheckoutSession.dto';
 import {getLineItems} from './helpers/getLineItems';
@@ -17,33 +18,45 @@ export class StripeService {
     this.stripe = new Stripe(this.apiKey);
   }
 
+  async findOrCreateCustomer(orderInformation: BillingInfo, userId: string): Promise<Stripe.Customer> {
+    const existingCustomers = await this.stripe.customers.list({
+      email: orderInformation.email,
+      limit: 1
+    });
+
+    if (existingCustomers.data.length > 0) {
+      return existingCustomers.data[0];
+    }
+
+    return await this.stripe.customers.create({
+      name: `${orderInformation.firstName} ${orderInformation.lastName}`,
+      email: orderInformation.email,
+      metadata: {
+        userId
+      }
+    });
+  }
+
   async createCheckoutSession(
-    {products, orderInformation, totalPrice}: CreateCheckoutSessionDto,
+    {products, orderInformation}: CreateCheckoutSessionDto,
     userId: string
   ): Promise<StripeSessionI> {
     const productsDetails = await this.cartService.getCart(userId);
     const line_items = getLineItems(productsDetails);
+    const customer = await this.findOrCreateCustomer(orderInformation, userId);
 
-    const customer = await this.stripe.customers.create({
-      name: `${orderInformation.firstName} ${orderInformation.lastName}`,
-      email: orderInformation.email,
-      metadata: {
-        userId,
-        products: JSON.stringify(products),
-        orderInformation: JSON.stringify(orderInformation),
-        totalPrice
-      }
-    });
-
-    const session = await this.stripe.checkout.sessions.create({
+    return await this.stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items,
       mode: 'payment',
       customer: customer.id,
+      metadata: {
+        products: JSON.stringify(products),
+        orderInformation: JSON.stringify(orderInformation),
+        userId
+      },
       success_url: `${this.configService.get('CLIENT_URL')}/success`,
       cancel_url: `${this.configService.get('CLIENT_URL')}/shopping-cart`
     });
-
-    return session;
   }
 }
